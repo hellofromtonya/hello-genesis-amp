ef31ab4f48216f92d490831c39bbfe6cfcf21305
Tonya Mork <hellofromtonya@knowthecode.io>
2019-04-12 17:24:56 -0500

    Adding latest posts to front page.

--- front-page.php
+++ front-page.php
@@ -18,4 +18,35 @@ remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
 render_hero();
 
+add_shortcode( 'latest_from_my_blog', __NAMESPACE__ . '\build_latest_from_my_blog_shortcode' );
+function build_latest_from_my_blog_shortcode( $attributes ) {
+	$defaults   = [
+		'post_type'      => 'post',
+		'posts_per_page' => 4,
+	];
+	$attributes = shortcode_atts( $defaults, $attributes, 'latest_from_my_blog' );
+
+	remove_filter( 'genesis_attr_entry-header', 'Hello_From_Tonya\Hello_Genesis_AMP\Structure\add_hero_section_attribute' );
+	add_action( 'genesis_entry_header', 'genesis_do_post_title' );
+	remove_action( 'genesis_entry_header', 'Hello_From_Tonya\Hello_Genesis_AMP\Structure\render_hero_content' );
+	add_filter( 'genesis_post_title_output', __NAMESPACE__ . '\change_post_title_to_h3' );
+	function change_post_title_to_h3( $html ) {
+
+		$html = str_replace( '<h2', '<h3', $html );
+
+		return str_replace( '</h2>', '</h3>', $html );
+	}
+	remove_action( 'genesis_after_endwhile', 'genesis_posts_nav' );
+
+	ob_start();
+	genesis_custom_loop( $attributes );
+	$html = ob_get_clean();
+
+	// Reset it all back.
+	add_action( 'genesis_after_endwhile', 'genesis_posts_nav' );
+	remove_filter( 'genesis_post_title_output', __NAMESPACE__ . '\change_post_title_to_h3' );
+
+	return $html;
+}
+
 genesis();
